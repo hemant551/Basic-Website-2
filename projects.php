77137b6fd1ed561b85759c03411f84c5f17f2c09
Jamie <jamieflargm@icloud.com>
2014-03-03 21:12:31 +0000

    General Fixes

--- projects.php
+++ projects.php
@@ -22,6 +22,7 @@ include("init/data.php");
             <nav id="main_menu">		        <?php include("init/navigation.php"); ?>	        </nav>
 
             <section id="main_section">
+
                 <article>
                     <header>
                         <h2>Minecraft Version Installer</h2>
@@ -30,14 +31,25 @@ include("init/data.php");
                         Available on <a href="https://github.com/lexware/Minecraft-Version-Installer" title="Github">Github</a>
                     </footer>
                 </article>
+
                 <article>
                     <header>
                         <h2>FreeOS live development</h2>
                     </header>
                     <footer>
-                        Available on <a href="http://scratch.mit.edu/projects/18028834" title="Github">Scratch</a>
+                        Available on <a href="http://scratch.mit.edu/projects/18028834" title="Scratch">Scratch</a>
                     </footer>
                 </article>
+
+                <article>
+                    <header>
+                        <h2>lexOS Beta 1.1</h2>
+                    </header>
+                    <footer>
+                        Available on <a href="http://scratch.mit.edu/projects/17937756" title="Scratch">Scratch</a>
+                    </footer>
+                </article>
+
             </section>
 
             <footer id="footer">
