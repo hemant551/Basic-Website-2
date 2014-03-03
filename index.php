94a9ef51b6c094f5267e15d0f5854967a98f12d5
Jamie <jamieflargm@icloud.com>
2014-03-03 19:11:24 +0000

    Wooo!
    Got the website structure sorted. and added 2 images

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include("init/data.php");    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?php echo $title?></title>
+        <link rel="stylesheet" type="text/css" href="/style/main.css">
+    </head>
+    <body>
+
+        <header id="header">
+            <a href="http://scratch.mit.edu/users/malgm" title="malgm on Scratch"><img src="/res/logo_malgm.png" alt="malgm logo" width="100" height="100" /></a>
+            <a href="http://scratch.mit.edu/users/lexOS" title="lexOS on Scratch"><img src="/res/logo_lexos.png" alt="lexos logo" width="100" height="100" /></a>
+            <h1>Lexware</h1>
+        </header>
+        
+    </body>
+</html>
